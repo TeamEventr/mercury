286f0b8c90668ac4a89cb0d9fc1574a7a3f8fbb6
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-02-01 17:38:57 +0530

    Payment route structure added

--- routes/auth.go
+++ routes/auth.go
@@ -1,22 +1,23 @@
 package routes
 
 import (
+	"github.com/IAmRiteshKoushik/mercury/controllers"
 	"github.com/IAmRiteshKoushik/mercury/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func AuthRoutes(engine *gin.RouterGroup) {
 
-	engine.POST("/auth/user/check")
-	engine.POST("/auth/user/login")
-	engine.POST("/auth/user/register")
-	engine.POST("/auth/user/register/otp/verify", middleware.AuthMiddleware())
-	engine.POST("/auth/user/register/otp/resend", middleware.AuthMiddleware())
+	engine.POST("/auth/user/check", controllers.CheckUsername)
+	engine.POST("/auth/user/login", controllers.LoginUser)
+	engine.POST("/auth/user/register", controllers.RegisterUserAccount)
+	engine.POST("/auth/user/register/otp/verify", middleware.AuthMiddleware(), controllers.VerifyUserOtp)
+	engine.POST("/auth/user/register/otp/resend", middleware.AuthMiddleware(), controllers.ResendUserOtp)
 
 	engine.GET("/auth/google/")
 	engine.GET("/auth/google/callback")
 
-	engine.POST("/auth/host/register")
-	engine.POST("/auth/host/register/otp/verify", middleware.AuthMiddleware())
-	engine.POST("/auth/host/register/otp/resend", middleware.AuthMiddleware())
+	engine.POST("/auth/host/register", controllers.RegisterHostAccount)
+	engine.POST("/auth/host/register/otp/verify", middleware.AuthMiddleware(), controllers.VerifyHostOtp)
+	engine.POST("/auth/host/register/otp/resend", middleware.AuthMiddleware(), controllers.ResendHostOtp)
 }
