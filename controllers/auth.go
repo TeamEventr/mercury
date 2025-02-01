522d3a1a42146e0803c383c0b36936fb7d79583e
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-02-01 17:39:09 +0530

    Payment route controllers defined

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,1 +1,35 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func CheckUsername(c *gin.Context) {
+
+}
+
+func RegisterUserAccount(c *gin.Context) {
+
+}
+
+func ResendUserOtp(c *gin.Context) {
+
+}
+
+func VerifyUserOtp(c *gin.Context) {
+
+}
+
+func LoginUser(c *gin.Context) {
+
+}
+
+func RegisterHostAccount(c *gin.Context) {
+
+}
+
+func VerifyHostOtp(c *gin.Context) {
+
+}
+
+func ResendHostOtp(c *gin.Context) {
+
+}
