9cd4ab2a2d1a9fa7e895b258d6f91ddce216e484
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-02-14 16:11:19 +0530

    Environment variables validation complete

--- cmd/env.go
+++ cmd/env.go
@@ -8,51 +8,85 @@ import (
 )
 
 type EnvConfig struct {
-	Environment string `default:"development" required:"true"`
-	Port        int    `default:"9000" required:"true"`
-	Db          string `default:"postgresql://postgres:1234@localhost:5432/" required:"true"`
+	Environment   string `default:"development" required:"true"`
+	Port          int    `default:"9000" required:"true"`
+	DBUrl         string `default:"postgresql://postgres:1234@localhost:5432/" required:"true"`
+	RzpKey        string
+	RzpSecret     string
+	OAuthClient   string
+	OAuthSecret   string
+	OAuthCallback string
+	CacheAddr     string
+	CachePwd      string
 }
 
-func (e *EnvConfig) NewEnv() (*EnvConfig, error) {
+func (e *EnvConfig) NewEnvConfig() (*EnvConfig, error) {
 	cfg := &EnvConfig{}
 	validEnvs := []string{"development", "testing", "production"}
 
-	envStr := os.Getenv("ENVIRONMENT")
-	portStr := os.Getenv("PORT")
-	database := os.Getenv("DATABASE_URL")
+	environment := os.Getenv("ENVIRONMENT")
+	port := os.Getenv("PORT")
+	dbUrl := os.Getenv("DATABASE_URL")
+	rzpKey := os.Getenv("RAZORPAY_CLIENT_ID")
+	rzpSecret := os.Getenv("RAZORPAY_CLIENT_SECRET")
+	oauthClientId := os.Getenv("OAUTH_CLIENT_ID")
+	oauthClientSecret := os.Getenv("OAUTH_CLIENT_SECRET")
+	oauthCallback := os.Getenv("OAUTH_CALLBACK_URL")
+	cacheAddr := os.Getenv("REDIS_URL")
+	cachePwd := os.Getenv("REDIS_PASSWORD")
 
-	// Validating and setting ENVIRONMENT
-	if envStr == "" {
-		return nil, fmt.Errorf("ENVIRONMENT environment variable is missing.")
-	}
-	envStr = strings.ToLower(envStr)
+	environment = strings.ToLower(environment)
 	isValid := false
 	for _, validEnv := range validEnvs {
-		if envStr == validEnv {
+		if environment == validEnv {
 			isValid = true
 			break
 		}
 	}
 	if !isValid {
-		return nil, fmt.Errorf("Invalid ENVIRONMENT value: %s", envStr)
+		return nil, fmt.Errorf("Invalid ENVIRONMENT value: %s", environment)
 	}
-	cfg.Environment = envStr
-
-	// Validating and setting PORT
-	if portStr == "" {
+	cfg.Environment = environment
+	if port == "" {
 		return nil, fmt.Errorf("PORT environment variable is missing.")
 	}
-	port, err := strconv.Atoi(portStr)
+	portNum, err := strconv.Atoi(port)
 	if err != nil {
 		return nil, fmt.Errorf("Invalid PORT value: %w", err)
 	}
-	cfg.Port = port
-
-	// Validating and setting DATABASE_URL
-	if database == "" {
-		return nil, fmt.Errorf("DATABASE environment variable is missing.")
+	cfg.Port = portNum
+	if dbUrl == "" {
+		return nil, fmt.Errorf("PORT environment variable is missing.")
 	}
-	cfg.Db = database
+	cfg.DBUrl = dbUrl
+	if rzpKey == "" {
+		return nil, fmt.Errorf("DATABASE_URL environment variable is missing.")
+	}
+	cfg.RzpKey = rzpKey
+	if rzpSecret == "" {
+		return nil, fmt.Errorf("RAZORPAY_SECRET environment variable is missing.")
+	}
+	cfg.RzpSecret = rzpSecret
+	if oauthClientId == "" {
+		return nil, fmt.Errorf("RAZORPAY_CLIENT_ID environment variable is missing.")
+	}
+	cfg.OAuthClient = oauthClientId
+	if oauthClientSecret == "" {
+		return nil, fmt.Errorf("OAUTH_CLIENT_SECRET environment variable is missing.")
+	}
+	cfg.OAuthSecret = oauthClientSecret
+	if oauthCallback == "" {
+		return nil, fmt.Errorf("OAUTH_CALLBACK_URL environment variable is missing.")
+	}
+	cfg.OAuthCallback = oauthCallback
+	if cacheAddr == "" {
+		return nil, fmt.Errorf("REDIS_URL environment variable is missing.")
+	}
+	cfg.CacheAddr = cacheAddr
+	if cachePwd == "" {
+		return nil, fmt.Errorf("REDIS_PASSWORD environment variable is missing.")
+	}
+	cfg.CachePwd = cachePwd
 
 	return cfg, nil
 }
