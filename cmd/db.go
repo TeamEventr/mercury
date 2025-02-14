e6625e0602e0e40f59194e4af2609fca88ffbe9b
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-02-14 15:17:55 +0530

    Handle DB init

--- cmd/db.go
+++ cmd/db.go
@@ -3,27 +3,43 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"os"
+	"time"
+
 	"github.com/jackc/pgx/v5/pgxpool"
-	"log"
 )
 
-func InitDB() {
-	// Fix the connection string
-	connPool, err := pgxpool.NewWithConfig(context.Background(), DBConfig(""))
+func InitDB() (*pgxpool.Pool, error) {
+	connStr := os.Getenv("DATABASE_URL")
+	poolConfig, err := pgxpool.ParseConfig(connStr)
 	if err != nil {
-		log.Fatal("Error while creating connection to the database!!")
+		// TODO: Setup logging when this happens (FATAL)
+		return nil, fmt.Errorf("Failed to parse connection string: %w", err)
 	}
 
-	// Acquire a connection from the pool
-	connection, err := connPool.Acquire(context.Background())
-	if err != nil {
-		log.Fatal("Error while acquiring connection from database pool!")
-	}
-	defer connection.Release()
+	// Configure Pool Settings
+	poolConfig.MaxConns = 10
+	poolConfig.MinConns = 10
+	poolConfig.MaxConnLifetime = time.Hour / 2
 
-	err = connection.Ping(context.Background())
+	pool, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
 	if err != nil {
-		log.Fatal("Could not PING database")
+		return nil, fmt.Errorf("Failed to create connection pool: %w", err)
 	}
-	fmt.Println("Connected to the database")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := pool.Acquire(ctx) // Acquire a connection to test
+	if err != nil {
+		// TODO: Setup logging when this happens (FATAL)
+		return nil, fmt.Errorf("Failed to acquire connection from pool: %w", err)
+	}
+	defer conn.Release()
+
+	if err := conn.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("Database connection test failed: %w", err)
+	}
+	fmt.Println("Database connection test successful.")
+	return pool, nil
 }
