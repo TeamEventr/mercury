d9803b7b83d0a88baf6d4df1476b049c1ac4ed60
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-03-20 16:09:34 +0530

    add: cf-kv cache system

---
+++ kv/events.go
@@ -0,0 +1,13 @@
+package kv
+
+func AddEvent(id, title string, ttl int) error {
+	return nil
+}
+
+func DeleteEvent(id, title string) error {
+	return nil
+}
+
+func FetchAllEvents() error {
+	return nil
+}
---
+++ kv/users.go
@@ -0,0 +1,25 @@
+package kv
+
+func AddUser(username, email string) error {
+	return nil
+}
+
+func RemoveUser(username string) error {
+	return nil
+}
+
+func CheckUsername(username string) error {
+	return nil
+}
+
+func GetCustomerSession(username string) error {
+	return nil
+}
+
+func GetHostSession(username string) error {
+	return nil
+}
+
+func GetStaffSession(username string) error {
+	return nil
+}
